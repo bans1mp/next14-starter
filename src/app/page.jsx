82369ab0d18af02b6eaec36b3bc3f012c3984864
bans1mp <iamsarthakgandotra@gmail.com>
2024-01-01 23:16:36 +0530

    app router setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,9 @@
-const Home = () => {
-  return <div>Hello World!</div>;
-};
+const HomePage = () => {
+  return (
+    <div>
+      HomePage      
+    </div>
+  )
+}
 
-export default Home;
+export default HomePage

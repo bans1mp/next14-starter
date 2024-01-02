6421f4eb31b9239028599673880d66806951da03
bans1mp <iamsarthakgandotra@gmail.com>
2024-01-02 22:23:33 +0530

    Navbar styling complete

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,29 +1,56 @@
-import Link from 'next/link'
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import styles from "./links.module.css"
 import NavLink from './navlink/NavLink'
 const Links = () => {
-
-    const links = [
-        {
+  const links = [
+    {
             title: "Home",
             path: '/'
+          },
+        {
+          title: "About",
+          path: '/about'
         },
         {
-            title: "About",
-            path: '/about'
-        },
-        {
-            title: "Contact",
-            path: '/contact'
+          title: "Contact",
+          path: '/contact'
         }
-    ]
+      ]
+      const [open , setOpen] = useState(false) ;
+      
+      const session = true ;
+    const isAdmin = true ;
 
   return (
+    <div className={styles.container}>
+
     <div className={styles.links}>
       {links.map((link) => {
         return <NavLink linkItem={link} key={link.path}/>
       })}
+      {session ? (
+        <>
+        {
+          isAdmin && (
+            <NavLink linkItem={{title: "Admin" , path: "/admin"}}/>
+            )
+          }
+        <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink linkItem={{title: "Login", path: "/login"}} />
+        )}
+    </div>
+    <button onClick={() => setOpen((prev) => !prev)} >Menu</button>
+    {
+      open && 
+      <div className={styles.mobileLinks}>
+        {links.map((link) => {
+          return <NavLink key={link.path} linkItem={link}/>
+        })}
+      </div>
+    }
     </div>
   )
 }
